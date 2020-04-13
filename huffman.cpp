40f944c4ece972da44d5e57d4c2dd24e8b947e5f
Allan-Perez <allan.steven.pm@gmail.com>
2020-04-13 22:29:35 +0100

    huffman. Progress up to building the pair vector with characters and frequency. TODO: build tree.

---
+++ huffman.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argv, char ** argc){
+  if(argv < 2){
+    cout << "I need a message to encode." << endl;
+    return 1;
+  }
+  string message = argc[1];
+
+  
+
+}
+
+int 
