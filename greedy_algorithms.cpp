5f2003ec220c010d6721bfc7fe0e06fb38210a6a
Allan-Perez <allan.steven.pm@gmail.com>
2020-04-13 21:58:48 +0100

    greedy search for time table

--- greedy_algorithms.cpp
+++ greedy_algorithms.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void greedy_coins(int, vector<int>&, vector<int>&);
 void print_vector(vector<int>);
+void greedy_schedule(vector<vector<int>>& events, vector<int> &scheadule);
 
 int main(int argv, char **argc){
   // In a nutshell: Choose the apparently best option at each step. 
@@ -28,7 +29,7 @@ int main(int argv, char **argc){
   greedy_coins(amount, n_coins, euro_coins);
   cout << "Number of coins: " << n_coins.size() << endl;
   cout << "The coins needed: " << endl;
-  print_vector(n_coins);
+  //print_vector(n_coins);
 
   // A way of seeing how many greedy algorithms can be there for a single problem
   // is throught the scheaduling problem. If you have n events that have a starting
@@ -44,24 +45,41 @@ int main(int argv, char **argc){
   //     is proven to be the one that yields the optimal value always. 
   // Here's an attempt to implement this problem
   // Scheduling
-  vector<vector<int, int>> events = {
-    { 1, 3 }
-    { 2, 5 }
-    { 3, 9 }
+  vector<vector<int>> events{ { 1, 3 },
+    { 2, 5 },
+    { 3, 9 },
     { 6, 8 }
   };
   vector<int> schedule;
+  greedy_schedule(events, schedule);
+  cout << "The optimal scheadule: " << endl;
+  print_vector(schedule);
 
+  // The more general problem of having n tasks with deadlines and durations
+  // and a score that depends on the deadline and the days it took to complete
+  // after the deadline (d-x) happens to have a similar optimal greedy solution: to always 
+  // pick first the tasks with less duration, regardless of the deadline. 
+  // This is because, given two successive tasks, if we do first the shortest
+  // we will have a better score than doing the opposite, regardless of the deadline.
 
   return 0;
 }
 
-void greedy_schedule(vector<vector<int,int>>& events, vector<int> &scheadule){
+void greedy_schedule(vector<vector<int>>& events, vector<int> &scheadule){
   // Sort events by ending time (second item)
   // start from the first element after sorting (the one ending the soonest)
   // then look for the next one ending the soonest that hasn't already started
   // keep going until there's no more such events.  
-  
+  sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b){
+    return a[1] < b[1];
+  });
+
+  scheadule.push_back(events[0][1]); // scheadule vector stores ending time of tasks
+  for(int i=1; i<events.size() ; i++){
+    if(events[i][0] >= scheadule.back()){
+      scheadule.push_back(events[i][1]);
+    }
+  }
 }
 
 void greedy_coins(int amount, vector<int> &n_coins, vector<int> &set_coins){
